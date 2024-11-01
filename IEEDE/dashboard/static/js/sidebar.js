29acedec81bb6617e95cab529878c4bb98f7bf81
rajeshmondal00 <rajeshmondalmain@gmail.com>
2024-11-01 19:12:14 +0530

    add new template files

--- IEEDE/dashboard/static/js/sidebar.js
+++ IEEDE/dashboard/static/js/sidebar.js
@@ -6,4 +6,37 @@ $(document).ready(function () {
     $(".aspan").toggleClass("a-span");
     $(".side-a").toggleClass("sidea");
   });
+  function checkWidth() {
+    if ($(window).width() < 480) {
+      $("#barswitch").click(function () {
+        $(".sidebar").toggleClass("mobile-side-expand");
+        $(".sidebar").removeClass("sidebar-collapse");
+        $(".main-body").removeClass("expand");
+        $("#logo").removeClass("logoCollapse");
+        $(".aspan").removeClass("a-span");
+        $(".side-a").removeClass("sidea");
+      });
+      $(document)
+        .off("click", "#close-bar")
+        .on("click", "#close-bar", function () {
+          $(".sidebar").removeClass("mobile-side-expand");
+        });
+    }
+  }
+  checkWidth();
+  $(window).resize(checkWidth);
+
+   let wasBelow480 = $(window).width() < 480;
+
+   $(window).resize(function () {
+     const isBelow480 = $(window).width() < 480;
+
+     // Check if the screen width has crossed the 480px threshold in either direction
+     if (wasBelow480 !== isBelow480) {
+       location.reload();
+     }
+
+     // Update the tracking variable for future checks
+     wasBelow480 = isBelow480;
+   });
 });
